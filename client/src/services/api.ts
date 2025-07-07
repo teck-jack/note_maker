a0cde11069da8dd418da53a8f1ea6ec8dc061e28
Monesh Sahu <119479121+teck-jack@users.noreply.github.com>
2025-07-07 21:41:44 +0530

    Update api.ts2

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://note-maker-1-hduy.onrender.com/';
+const API_BASE_URL = 'https://note-maker-1-hduy.onrender.com/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
